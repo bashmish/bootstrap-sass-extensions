8f4c9baa480e4e95dcfed41e20e5dc3d467efde1
Michael Bashkirov <bashmish@gmail.com>
2013-05-13 15:44:07 +0400

    Appropriate highlighter for several words in query.

--- vendor/assets/javascripts/bootstrap-typeahead.js
+++ vendor/assets/javascripts/bootstrap-typeahead.js
@@ -151,10 +151,14 @@
     }
 
   , highlighter: function (item) {
-      var query = this.query.replace(/[\-\[\]{}()*+?.,\\\^$|#\s]/g, '\\$&')
-      return item.replace(new RegExp('(' + query + ')', 'ig'), function ($1, match) {
-        return '<strong>' + match + '</strong>'
-      })
+      var words = $.trim(this.query).split(' ')
+      for(var i in words) {
+        var word = words[i].replace(/[\-\[\]{}()*+?.,\\\^$|#\s]/g, '\\$&')
+        item = item.replace(new RegExp('(' + word + ')', 'ig'), function ($1, match) {
+          return '<strong>' + match + '</strong>'
+        })
+      }
+      return item
     }
 
   , render: function (items) {
